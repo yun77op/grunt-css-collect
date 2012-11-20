2a1896c94f65b704378484466a53eb9e84e857cb
yun77op <yun77op@gmail.com>
2012-11-20 16:51:52 +0800

    Fix ftl is not valid html
    Add regexp replace method to replace link. User can config which method
    to use, regexp replace or cheerio.

--- tasks/html_substitute.js
+++ tasks/html_substitute.js
@@ -7,71 +7,115 @@
 
 var Path = require('path');
 var os = require('os');
-var cheerio = require('cheerio');
 
 module.exports = function(grunt) {
-  "use strict";
+    "use strict";
 
-  grunt.util = grunt.util || grunt.utils;
+    grunt.util = grunt.util || grunt.utils;
 
-  var _ = grunt.util._;
-  var File = grunt.file;
+    var _ = grunt.util._;
+    var File = grunt.file;
 
-  // ==========================================================================
-  // TASKS
-  // ==========================================================================
 
-  grunt.registerMultiTask("html_substitute", "Substitue assets link in html", function() {
-    var config = _.defaults({}, this.data);
+    var relStylesheetPattern = /rel\s*=\s*(['"]?)stylesheet\1/;
+    var stylesheetHrefPattern = /href\s*=\s*(['"]?)([^>\s'"]+)\1/;
 
-    var resourceMap = {};
-    var resourcePathMap = config.resource_map;
+    var processers = {
+        dom: function(source, resourceMap) {
+            var cheerio = require('cheerio');
+            var $ = cheerio.load(source);
 
-    if (!Array.isArray(resourcePathMap)) {
-      resourcePathMap = [resourcePathMap]; 
-    }
+            $('link[rel=stylesheet]').each(function(idx, elm) {
+                var $elm = $(elm);
+                var href_src = $elm.attr('href');
+                var href_dst = resourceMap[href_src];
 
-    resourcePathMap.forEach(function(filepath) {
-      filepath = grunt.template.process(filepath);
-      var map = File.readJSON(filepath);
-      _.extend(resourceMap, map);
+                if (!href_dst) {
+                    grunt.fail.warn('Failed to substitue ' + href_src + '.');
+                }
+
+                $elm.attr('href', href_dst);
+            });
+
+            return $.html();
+        },
+
+        line: function(source, resourceMap) {
+            var lines = source.replace(/\r/g, '').split('\n');
+
+            function isStylesheetLine(line) {
+                line = line.trim();
+
+                if (line.indexOf('<link') != 0 ||
+                    !line.match(relStylesheetPattern)) return false;
+
+                return true;
+            }
+
+            lines = lines.map(function(line) {
+                if (!isStylesheetLine(line)) return line;
+
+                return line.replace(stylesheetHrefPattern, function(full, quotes, path) {
+                    var value = resourceMap[path];
+
+                    if (!value) {
+                        grunt.fail.warn('Failed to substitue ' + path + '.');
+                    }
+
+                    return 'href="' + value + '"';
+                });
+            });
+
+            return lines.join(os.EOL);
+        }
+    };
+
+    // ==========================================================================
+    // TASKS
+    // ==========================================================================
+
+    grunt.registerMultiTask("html_substitute", "Substitue assets link in html", function() {
+        var config = _.defaults({}, this.data);
+
+        var resourceMap = {};
+        var resourcePathMap = config.resource_map;
+
+        if (!Array.isArray(resourcePathMap)) {
+            resourcePathMap = [resourcePathMap];
+        }
+
+        resourcePathMap.forEach(function(filepath) {
+            filepath = grunt.template.process(filepath);
+            var map = File.readJSON(filepath);
+            _.extend(resourceMap, map);
+        });
+
+        config.resourceMap = resourceMap;
+
+        var files = config.files;
+
+        if (!Array.isArray(files)) {
+            files = [files];
+        }
+
+        files = _(files).chain().map(function(file) {
+            file = Path.join(config.src, file);
+            return File.expandFiles(file);
+        }).flatten().value();
+
+        files.forEach(function(file) {
+            grunt.helper('html_substitute', file, config);
+        });
+
+        grunt.log.ok();
     });
 
-    var files = config.files;
+    grunt.registerHelper('html_substitute', function(filepath, config) {
+        var source = String(File.read(filepath, 'utf-8'));
+        var processer = processers[config.process_type || 'line'];
+        var html = processer(source, config.resourceMap);
 
-    if (!Array.isArray(files)) {
-      files = [files];
-    }
-
-    files = _(files).chain().map(function(file) {
-      file = Path.join(config.src, file);
-      return File.expandFiles(file);
-    }).flatten().value();
-
-    files.forEach(function(file) {
-      grunt.helper('html_substitute', file, resourceMap);
+        File.write(filepath, html);
     });
-
-    grunt.log.ok();
-  });
-
-  grunt.registerHelper('html_substitute', function(filepath, resourceMap) {
-    var source = String(File.read(filepath, 'utf-8'));
-    var $ = cheerio.load(source);
-
-    $('link[rel=stylesheet]').each(function(idx, elm) {
-      var $elm = $(elm);
-      var href_src = $elm.attr('href');
-      var href_dst = resourceMap[href_src];
-
-      if (!href_dst) {
-        grunt.fail.warn('Failed to substitue ' + href_src + '.');
-      }
-
-      $elm.attr('href', href_dst);
-    });
-
-    File.write(filepath, $.html());
-  });
 
 };
